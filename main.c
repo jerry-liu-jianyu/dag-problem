eb67a05ff2af80f05c40add015faabbf6760ae91
jliu <jerry_leo@msn.com>
2023-12-17 22:30:00 +0800

    1. Implemented is_dag with Depth First Search (DFS), 2023-12-17, jliu

--- main.c
+++ main.c
@@ -3,16 +3,23 @@
 #include <stdlib.h>
 #include "graph_utils.h"
 
+#define initial  1
+#define visited  2
+#define finished 3
+
 /*
  * Forward declarations of is_dag
  * See function definition further down for documentation
  */
 int is_dag(uint32_t digraph[], int n_vertices);
 
+int DFS(int n, uint32_t digraph[], int n_vertices, int state[]);
 
 int main(int argc, const char **argv)
 {
 	int i;
+	uint32_t *digraph;
+        int n_vertices;
 
 	if (argc <= 1) {
 		fprintf(stderr, "\nUsage: is_dag <digraph file>+\n\n");
@@ -20,9 +27,8 @@ int main(int argc, const char **argv)
 	}
 
 	for (i = 1; i < argc; i++) {
-		uint32_t *digraph;
-		int n_vertices;
-
+                digraph = NULL;
+		n_vertices = 0;
 		if (read_graph(argv[i], &digraph, &n_vertices)) {
 			fprintf(stderr, "Error reading digraph from file %s\n", argv[i]);
 			return 1;
@@ -35,7 +41,6 @@ int main(int argc, const char **argv)
 
 	return 0;
 }
-
 
 /********************************************************************************
  *
@@ -54,7 +59,51 @@ int main(int argc, const char **argv)
  * through 31, the digraph can have at most 32 vertices.
  *
  ********************************************************************************/
-int is_dag(uint32_t digraph[], int n_vertices)
+
+int is_dag(uint32_t digraph[], int n_vertices) 
 {
-	return 0;
+  int i; 
+  int dag = 1 ;
+
+  int *state;
+
+  state = (int*) malloc(n_vertices * sizeof(int));
+  if (state == NULL) {
+     printf("Memory not allocated.\n");
+     exit(-1);
+  }
+
+  for(i=0; i < n_vertices; i++) state[i] = initial;
+
+  /* statr DFS from vertex 0 */
+  dag = DFS(0, digraph, n_vertices, state); 
+
+  for ( i = 0; i < n_vertices; i++ ) { 
+    if (state[i] == initial )  dag = DFS(i, digraph, n_vertices, state);
+    if ( dag == 0 ) break;
+  }
+  free(state);
+  return dag; 
+}
+
+int DFS(int n, uint32_t digraph[], int n_vertices, int state[])
+{
+  int i;
+  int dag = 1;
+
+  state[n] = visited;
+
+  for ( i=0; i < n_vertices; i++ ) {
+    if ( digraph[n] & ( 1 << i ) ) {
+      if (state[i] == initial ) {
+        dag = DFS(i, digraph, n_vertices, state);
+      }
+      else if ( state[i] == visited ) {  
+        dag = 0;
+        break;     
+      }
+    }
+  }
+  state[n] = finished; 
+  return dag;
 }
